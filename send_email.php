88d0f8edcb28527b48e8641c7abb9e5216fdc026
Sylvian-Brunet <sylvian84@hotmail.fr>
2019-11-24 18:35:35 +0100

    Commit du site + envoi de l'email

--- send_email.php
+++ send_email.php
@@ -13,29 +13,43 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+
+    $mail->CharSet      = 'UTF-8';
+    $mail->SMTPDebug    = 0;                            // Enable verbose debug output
+    $mail->isSMTP();                                    // Set mailer to use SMTP
+    $mail->Host         = 'auth.smtp.1and1.fr';         // Specify main and backup SMTP servers
+    $mail->SMTPAuth     = true;                         // Enable SMTP authentication
+    $mail->Username     = 'test@asheart.fr';            // SMTP username
+    $mail->Password     = '@x8NfZB8zDSFRgU';            // SMTP password
+    $mail->SMTPSecure   = 'ssl';                        // Enable TLS encryption, `ssl` also accepted
+    $mail->Port         =  465;                         // TCP port to connect to
+
+
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     //$mail->isSMTP();                                            // Send using SMTP
-    $mail->Host = 'mail.websenso.net';                    // Set the SMTP server to send through
-    $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-    $mail->Username = 'miw@websenso.net';                     // SMTP username
-    $mail->Password = '%)g@{Z>75[6N';                               // SMTP password
-    //  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    //  $mail->Port       = 587;                                    // TCP port to connect to
+    //$mail->Host = 'mail.websenso.net';                          // Set the SMTP server to send through
+    //$mail->SMTPAuth = true;                                     // Enable SMTP authentication
+    //$mail->Username = 'miw@websenso.net';                       // SMTP username
+    //$mail->Password = '%)g@{Z>75[6N';                           // SMTP password
+    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    //$mail->Port       = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('miw@websenso.net', 'MIW Party');
-    $mail->addAddress('james+miw@websenso.com', 'James');     // Add a recipient
-    $mail->addCC('miw@websenso.net');
+    $email = $_GET['email'];
+    $mail->setFrom('test@asheart.fr', 'MIW Party');
+    $mail->addAddress($email);
+    //$mail->addAddress('james+miw@websenso.com', 'James');     // Add a recipient
+    //$mail->addCC('miw@websenso.net');
 
     // Attachments
-//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
+    $mail->Subject = 'XMAS Party ! Jeudi 19 dec. a Gap';
     $mail->Body = file_get_contents('email/email_party.html');
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->AltBody = 'Soirée XMAS Party Jeudi 19 decembre a Gap. Venez nombreux. Nous aurons en notre compagnie quatre superbe navigatrices !';
 
     $mail->send();
     //echo 'Message has been sent';
